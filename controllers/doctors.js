ede414e01c2034560d70afd60c55791b5804af2f
Nikola0707 <ristoski.nikola@gmail.com>
2021-04-19 23:22:23 +0200

    Created search bara with dropdown list for doctors

--- controllers/doctors.js
+++ controllers/doctors.js
@@ -1,51 +1,108 @@
-const Doctor = require('../models/doctor');
+const Doctor = require("../models/doctor");
 
 module.exports = {
-  getAll: async (req, res) => {
-    const doctors = await Doctor.find();
+  // getAll: async (req, res) => {
+  //   const doctors = await Doctor.find();
 
-    res.render('doctors/index', { doctors: doctors });
+  //   res.render('doctors/index', { doctors: doctors });
+  // },
+  getAll: function (req, res) {
+    if (req.query) {
+      const regex = new RegExp(req.query.search, "gi");
+      if (req.query.select === "city") {
+        Doctor.find({ $or: [{ city: regex }] }, function (err, allDoctors) {
+          if (err) {
+            console.log(err);
+          } else {
+            res.render("doctors/index", { doctors: allDoctors });
+          }
+        });
+      } else if (req.query.select === "doctorName") {
+        Doctor.find(
+          { $or: [{ full_name: regex }] },
+          function (err, allDoctors) {
+            if (err) {
+              console.log(err);
+            } else {
+              res.render("doctors/index", { doctors: allDoctors });
+            }
+          }
+        );
+      } else if (req.query.select === "specialization") {
+        Doctor.find(
+          { $or: [{ specialization: regex }] },
+          function (err, allDoctors) {
+            if (err) {
+              console.log(err);
+            } else {
+              res.render("doctors/index", { doctors: allDoctors });
+            }
+          }
+        );
+      } else {
+        Doctor.find(
+          { $or: [{ full_name: regex }, { city: regex }] },
+          function (err, allDoctors) {
+            if (err) {
+              console.log(err);
+            } else {
+              res.render("doctors/index", { doctors: allDoctors });
+            }
+          }
+        );
+      }
+    } else {
+      Doctor.find({}, function (err, allDoctors) {
+        if (err) {
+          console.log(err);
+        } else {
+          res.render("doctors/index", { doctors: allDoctors });
+        }
+      });
+    }
   },
   getOne: async (req, res) => {
-    const doctor = await Doctor.findById(req.params.id)
+    const doctor = await Doctor.findById(req.params.id);
 
-    res.render('doctors/update', doctor)
+    res.render("doctors/update", doctor);
   },
   create: (req, res) => {
-    res.render('doctors/create');
+    res.render("doctors/create");
   },
   postCreate: async (req, res) => {
     try {
-      const doctor = new Doctor(req.body)
-      await doctor.save()
+      const doctor = new Doctor(req.body);
+      await doctor.save();
 
-      res.redirect('/doctors')
+      res.redirect("/doctors");
     } catch (error) {
-      res.render('doctors/create', {
+      res.render("doctors/create", {
         ...req.body,
-        error: error.message
-      })
+        error: error.message,
+      });
     }
   },
   postUpdate: async (req, res) => {
     try {
-      await Doctor.findByIdAndUpdate(req.params.id, req.body, { runValidators: true });
-      res.redirect('/doctors');
+      await Doctor.findByIdAndUpdate(req.params.id, req.body, {
+        runValidators: true,
+      });
+      res.redirect("/doctors");
     } catch (error) {
-      res.render('doctors/update', {
+      res.render("doctors/update", {
         ...req.body,
         _id: req.params.id,
-        error: error.message
-      })
+        error: error.message,
+      });
     }
   },
   delete: async (req, res) => {
     // TODO: try catch
-    await Doctor.findByIdAndRemove(req.params.id)
+    await Doctor.findByIdAndRemove(req.params.id);
 
     res.send({
       error: false,
-      message: `Doctor with id #${req.params.id} removed`
+      message: `Doctor with id #${req.params.id} removed`,
     });
-  }
+  },
 };
